8ec4409f82129f76824c67438348417f74e71719
Celso Hamerski <ch19470426@gmail.com>
2026-07-01 09:11:56 -0300

    teste: remover layout.tsx tela31

--- src/app/vistoria/tela31/layout.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/vistoria/tela31/layout.tsx
-export const dynamic = 'force-dynamic'
-
-export default function Tela31Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-}
