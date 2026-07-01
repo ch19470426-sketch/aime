c817f664db269357b894cdcad7e540248933771c
Celso Hamerski <ch19470426@gmail.com>
2026-07-01 16:55:07 -0300

    feat: tela32 Vistoria Inspecao

---
+++ src/app/vistoria/tela32/layout.tsx
@@ -0,0 +1,6 @@
+// src/app/vistoria/tela31/layout.tsx
+export const dynamic = 'force-dynamic'
+
+export default function Tela31Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
