43970ebbf58935d11707cfb55601f697efbd4b13
ch19470426-sketch <ch19470426@gmail.com>
2026-07-08 17:30:50 -0300

    fix: force redeploy homologar

--- src/app/homologar/layout.tsx
+++ src/app/homologar/layout.tsx
@@ -1,3 +1,4 @@
+// src/app/homologar/layout.tsx
 export const dynamic = 'force-dynamic'
 export default function HomologarLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
