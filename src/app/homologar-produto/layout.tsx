e76f6855b3300bc19b348378c973c36e6f230001
ch19470426-sketch <ch19470426@gmail.com>
2026-07-14 08:54:46 -0300

    feat: layout da tela compartilhada homologar-produto (2.10)

---
+++ src/app/homologar-produto/layout.tsx
@@ -0,0 +1,5 @@
+// src/app/homologar-produto/layout.tsx
+export const dynamic = 'force-dynamic'
+export default function HomologarProdutoLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
